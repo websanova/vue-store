f1617bb4292c6a0aa204188107c1bbb3813b01fd
websanova <rob@websanova.com>
2016-10-03 13:07:04 +0700

    Support for Vue 2.0 (should be backward compatible).

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = (function () {
         data = data || {};
 
         for (key in data) {
-            store.$set(key, data[key]);
+            Vue.set(store, key, data[key]);
         }
 
         Object.defineProperties(Vue.prototype, {
@@ -14,9 +14,9 @@ module.exports = (function () {
                 get: function() {
                     var _this = this;
 
-                    store.set = function (key, val) { store.$set(key, val); return store; }
+                    store.set = function (key, val) { Vue.set(store, key, val); return store; }
                     store.watch = function (key, func) { store.$watch(key, func.bind(_this)); return store; }
-                    store.get = function (key) { return store.$get(key).bind(_this); }
+                    store.get = function (key) { return store[key].bind(_this); }
 
                     return store;
                 }
