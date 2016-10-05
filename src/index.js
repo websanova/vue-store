21f0a85c06253d96d5b5ea4aba88b521aba722a8
websanova <rob@websanova.com>
2016-10-05 14:20:24 +0700

    Fix up store to work with bind (and backward compatible).

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ module.exports = (function () {
         data = data || {};
 
         for (key in data) {
-            Vue.set(store, key, data[key]);
+            store[key] = data[key];
+        }
+
+        function indexGet(obj, i) { return obj[i]; }
+        
+        function indexSet(key, val, obj) {
+            if (key.length < 2) { 
+                obj[key[0]] = val;
+            } else {
+                if (!obj[key[0]]) obj[key[0]] = {};
+                
+                obj = obj[key.shift()];
+                
+                indexSet(key, val, obj);
+            }  
         }
 
         Object.defineProperties(Vue.prototype, {
@@ -14,9 +28,15 @@ module.exports = (function () {
                 get: function() {
                     var _this = this;
 
-                    store.set = function (key, val) { Vue.set(store, key, val); return store; }
+                    store.set = function (key, val) { indexSet(key.split('.'), val, store); return store; }
                     store.watch = function (key, func) { store.$watch(key, func.bind(_this)); return store; }
-                    store.get = function (key) { return store[key].bind(_this); }
+                    store.get = function (key) {
+                        var val = key.split('.').reduce(indexGet, store);
+
+                        if (typeof val === 'function') { return val.bind(_this); }
+
+                        return val;
+                    }
 
                     return store;
                 }
