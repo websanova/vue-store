e63aa36056cb11a4b79a34df69c2d6580c7d5d34
websanova <rob@websanova.com>
2017-05-19 20:55:27 +0700

    Update for dot notation and add "find" function.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,143 @@
 module.exports = (function () {
-    return function install(Vue, data) {
-        var key,
-            store = new Vue({
-                data: data || {}
-            });
 
-        function indexGet(obj, i) { return obj[i]; }
-        
-        function indexSet(key, val, obj) {
-            if (key.length < 2) { 
-                obj[key[0]] = val;
-            } else {
-                if (!obj[key[0]]) obj[key[0]] = {};
-                
-                obj = obj[key.shift()];
-                
-                indexSet(key, val, obj);
-            }  
+    function isObject(val) {
+        if (val !== null && typeof val === 'object' && val.constructor !== Array ) {
+            return true;
         }
+
+        return false;
+    }
+
+    function _toCamelCase(val) {
+        return val.replace(/^([A-Z])|[\s-_:](\w)/g, function(match, p1, p2, offset) {
+            if (p2) {
+                return p2.toUpperCase();
+            }
+            
+            return p1.toLowerCase();        
+        });
+    }
+
+    function _getObj(name) {
+        var i, ii, obj;
+
+        name = _init.call(this, name).split('.');
+
+        obj = this.watch.data;
+
+        for (i = 0, ii = name.length; i < ii; i++ ) {
+            obj = obj[name[i]];
+
+            if ( ! obj) {
+                return undefined;
+            }
+        }
+
+        return obj;
+    }
+
+    function _init(name) {
+        var i, ii, obj, _name;
+
+        _name = name.split('.');
+
+        obj = this.watch.data;
+
+        for (i = 0, ii = _name.length; i < ii; i++) {
+            if ( ! obj[_name[i]]) {
+
+                // If last one.
+                if (i + 1 === ii) {
+                    this.options.Vue.set(obj, _name[i], undefined);
+                }
+                else {
+                    this.options.Vue.set(obj, _name[i], {});
+                }
+            }
+
+            obj = obj[_name[i]];
+        }
+
+        return name;
+    }
+
+    function Toggle(values, options) {
+        this.options = options;
+
+        this.watch = new options.Vue({
+            data: function () {
+                return {
+                    data: values || {}
+                };
+            }
+        });
+    }
+
+    Toggle.prototype.set = function (key, val) {
+        var i, ii,
+            obj = this.watch.data,
+            name = _init.call(this, key).split('.');
+
+        for (i = 0, ii = name.length; i < ii; i++) {
+            if (i + 1 === ii) {
+                this.options.Vue.set(obj, name[i], val);
+            }
+            else {
+                obj = obj[name[i]];
+            }
+        }
+    };
+
+    Toggle.prototype.get = function (key) {
+        var obj = _getObj.call(this.$store, key);
+
+        if (typeof obj === 'function') {
+            return obj.bind(this);
+        }
+
+        return obj;
+    };
+
+    Toggle.prototype.find = function (haystack, needle, key) {
+        var i, ii,
+            obj = _getObj.call(this, haystack);
+
+        key = key || 'id';
+
+        if (obj.constructor === Array) {
+            for (i = 0, ii = obj.length; i < ii; i++) {
+                if (obj[i][key] === needle) {
+                    return obj[i];
+                }
+            }
+        }
+
+        else if (isObject(obj)) {
+            for (i in obj) {
+                if (obj[i][key] === needle) {
+                    return obj[i];
+                }
+            }
+        }
+
+        return undefined;
+    }
+
+    return function install(Vue, values, options) {
+        var store,
+            _get;
+
+        options = options || {};
+
+        options.Vue = Vue;
+
+        store = new Toggle(values, options),
+        _get = store.get;
 
         Object.defineProperties(Vue.prototype, {
             $store: {
                 get: function() {
-                    var _this = this;
-
-                    store.set = function (key, val) { indexSet(key.split('.'), val, store); return store; }
-                    store.watch = function (key, func) { store.$watch(key, func.bind(_this)); return store; }
-                    store.get = function (key) {
-                        var val = key.split('.').reduce(indexGet, store);
-
-                        if (typeof val === 'function') { return val.bind(_this); }
-
-                        return val;
-                    }
+                    store.get = _get.bind(this);
 
                     return store;
                 }
